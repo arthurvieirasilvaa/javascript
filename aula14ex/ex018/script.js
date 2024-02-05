4345334fa7e134944a4dc9b9b0702814af5b4d8b
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2024-02-05 15:50:17 -0300

    atualizei o ex018

---
+++ aula14ex/ex018/script.js
@@ -0,0 +1,19 @@
+function tabuada() {
+    let txtnum = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+    
+    if(txtnum.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }
+
+    else {
+        let num = Number(txtnum.value)
+        tab.innerHTML = ''
+        for(let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text += `${num} x ${i} = ${num * i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+}
