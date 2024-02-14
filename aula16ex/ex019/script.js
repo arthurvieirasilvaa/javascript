1aa8b4e8a9d2ce97707f452808e31541ce8c60cf
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2024-02-14 15:49:59 -0300

    finalizei o curso

--- aula16ex/ex019/script.js
+++ aula16ex/ex019/script.js
@@ -25,13 +25,18 @@ function adicionar() {
         let item = document.createElement('option')
         item.text += `Valor ${num} adicionado.`
         item.value = `${num}`
+        
         flista.appendChild(item)
         arr.push(num)
+        res.innerHTML = ''
     }
 
     else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+
+    txtnum.value = ''
+    txtnum.focus()
 }
 
 function maiorValor() {
@@ -66,16 +71,24 @@ function somar() {
 }
 
 function finalizar() {
-    res.innerHTML += `<p>Ao todo, temos ${arr.length} números cadastrados.</p>`
-    
-    let maior = maiorValor()
-    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-    
-    let menor = menorValor()
-    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+    if(arr.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    }
 
-    let soma = somar()
-    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    else {
+        res.innerHTML = ''
 
-    res.innerHTML += `<p>A média dos valores digitados é ${soma / arr.length}.</p>`
+        res.innerHTML += `<p>Ao todo, temos ${arr.length} números cadastrados.</p>`
+    
+        let maior = maiorValor()
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        
+        let menor = menorValor()
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+
+        let soma = somar()
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+
+        res.innerHTML += `<p>A média dos valores digitados é ${soma / arr.length}.</p>`
+    }
 }
